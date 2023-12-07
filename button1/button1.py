e1ace207bba56a814bb78891976ab3df48a81c23
Justin Wheeler <justin.wheeler@gfs.com>
2023-12-07 02:00:44 -0500

    updated button1

--- button1/button1.py
+++ button1/button1.py
@@ -6,10 +6,10 @@ def button_pressed(channel):
 
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_pressed)
+GPIO.add_event_detect(25, GPIO.RISING, callback=button_pressed)
 
 input("press enter to quit\n\n")
 
